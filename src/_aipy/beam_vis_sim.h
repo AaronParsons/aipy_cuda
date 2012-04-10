9a0669eb828f5d967273e1b11524eefec63f46ab
Gilbert Hsyu <ghsyu@berkeley.edu>
2012-04-10 12:53:03 -0700

    Initial commit of a version of vis_sim with beam calculations.  Not working yet.

---
+++ src/_aipy/beam_vis_sim.h
@@ -0,0 +1,20 @@
+#ifndef _VIS_SIM_H_
+#define _VIS_SIM_H_
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define  CL_M_PI_F          3.14159274101257f
+
+int vis_sim(float *, float *, float *, float *,
+            float *, float *, float *,
+            int, int, int, int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _VIS_SIM_H_ */
